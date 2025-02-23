617d98652b8e4b2020f51fb14a6a6289e44a78c0
Amit Mohite <amit.mohite@outlook.com>
2025-02-23 05:33:26 +0530

    version="0.1.7"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rim_weighting",
-    version="0.1.6",
+    version="0.1.7",
     author="Amit Mohite",
     author_email="amit.mohite@outlook.com",
     description="A Python implementation of the Random Iterative Method (RIM) weighting algorithm for survey data adjustment.",
